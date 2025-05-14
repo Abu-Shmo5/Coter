81d978ee061ae8639b894021e6a440298ad6f8d0
Abu Shmo5 <abushmo544@gmail.com>
2025-05-14 19:20:42 +0300

    notes pages

---
+++ web/src/app/notes/id/id.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-id',
+  imports: [],
+  templateUrl: './id.component.html',
+  styleUrl: './id.component.scss'
+})
+export class IdComponent {
+
+}
