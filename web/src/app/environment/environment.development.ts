00040d4607923aed3d92bd935d64a52667ccd248
Abu Shmo5 <abushmo544@gmail.com>
2025-05-14 19:18:36 +0300

    angular env file

---
+++ web/src/app/environment/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    ApiUrl: "http://coter.abushmo5.home:10000/api/",
+    tokenStorageName: "token",
+};
---
+++ web/src/app/environment/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    ApiUrl: "https://abu-shmo5.com/coter/api/",
+    tokenStorageName: "token",
+};
