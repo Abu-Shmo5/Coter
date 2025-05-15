148183e2cb9741ef60f9bc6c1173d9492cd6d58f
Abu Shmo5 <abushmo544@gmail.com>
2025-05-15 08:48:25 +0300

    store token

--- web/src/app/account/login/login.component.ts
+++ web/src/app/account/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { HttpResponse, HttpResponseBase } from '@angular/common/http';
+import { LocalstorageService } from '../../services/localstorage.service';
+import { environment } from '../../environment/environment';
 
 @Component({
   selector: 'app-login',
@@ -13,10 +16,20 @@ export class LoginComponent {
   username: string = ""
   password: string = ""
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private localstorage: LocalstorageService) {}
 
   login() {
-    console.log(this.username, this.password)
-    this.auth.login(this.username, this.password)
+    let response = this.auth.login(this.username, this.password)
+    response.subscribe({next: (result: any) => {
+      if (result['status'] == 200) {
+        if (result.body['Status'] == "Worked") {
+          this.localstorage.setItem(environment.tokenStorageName, result.body['Message'])
+          return
+        }
+        alert(result.body['Message'])
+      }
+    }, error: (error: any) => {
+      console.log(error)
+    }})
   }
 }
